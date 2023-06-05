9cf4c3b2106850d7ac5dd7dd663e5e1f9c881cdb
Shaun Maher <shaun@ghanima.net>
2023-06-05 07:38:37 +0000

    Felsh out entrypoint for client

--- pull-client/entrypoint.sh
+++ pull-client/entrypoint.sh
@@ -1,6 +1,12 @@
+#!/usr/bin/bash
+
+SSHD_PORT="${SSHD_PORT:-768}"
+CHISEL_REMOTE_URL="${CHISEL_REMOTE_URL:-"https://lu.ghanima.net/.chisel"}"
+CHISEL_REMOTES="${CHISEL_REMOTES:-"R:10022:localhost:10022"}"
+CHISEL_AUTH="${CHISEL_AUTH:-"user:password"}"
 
 # Start the SSH daemon
-
+/usr/sbin/sshd -p ${SSHD_PORT} -D -d -e &
 
 # Start the chisel tunnel
-chisel client -v https://lu.ghanima.net/.chisel R:10022:localhost:10022
+/usr/bin/chisel client -v --auth "${CHISEL_AUTH}" "${CHISEL_REMOTE_URL}" ${CHISEL_REMOTES}
