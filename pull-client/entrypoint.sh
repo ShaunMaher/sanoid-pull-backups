8ec33d18bd19aa56484f484ac150eb2078aacaad
Shaun Maher <shaun@ghanima.net>
2023-06-05 07:45:52 +0000

    Work in progress

--- pull-client/entrypoint.sh
+++ pull-client/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
+SSH_USERNAME="${SSH_USERNAME:-"syncoid"}"
 SSHD_PORT="${SSHD_PORT:-768}"
 CHISEL_REMOTE_URL="${CHISEL_REMOTE_URL:-"https://lu.ghanima.net/.chisel"}"
 CHISEL_REMOTES="${CHISEL_REMOTES:-"R:10022:localhost:10022"}"
 CHISEL_AUTH="${CHISEL_AUTH:-"user:password"}"
+
+useradd -m -d /var/lib/syncoid/${SSH_USERNAME} -u ${UID:-600} ${SSH_USERNAME}
+mkdir /var/lib/syncoid/${SSH_USERNAME}/.ssh
+chmod 700 /var/lib/syncoid/${SSH_USERNAME}/.ssh
+if [ "${SSH_PUBKEY}" != "" ]; then
+  echo "${SSH_PUBKEY}" > /var/lib/syncoid/${SSH_USERNAME}/.ssh/authorized_keys
+  chmod 600 /var/lib/syncoid/${SSH_USERNAME}/.ssh/authorized_keys
+fi
+
+mkdir /run/sshd
 
 # Start the SSH daemon
 /usr/sbin/sshd -p ${SSHD_PORT} -D -d -e &
--- pull-server/entrypoint.sh
+++ pull-server/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-useradd -m -d /var/lib/syncoid/${USERNAME} -u ${UID:-600} ${USERNAME}
-mkdir /var/lib/syncoid/${USERNAME}/.ssh
-chmod 700 /var/lib/syncoid/${USERNAME}/.ssh
-echo "${PUBKEY}" > /var/lib/syncoid/${USERNAME}/.ssh/authorized_keys
-chmod 600 /var/lib/syncoid/${USERNAME}/.ssh/authorized_keys
+#TODO: Start gitlab runner
 
-mkdir /run/sshd
-/usr/sbin/sshd
+#TODO: Then what?
