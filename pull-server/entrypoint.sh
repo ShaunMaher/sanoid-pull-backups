bb32d8bff73f55b542966db140069740fcbda9d9
Shaun Maher <shaun@ghanima.net>
2023-06-12 04:10:27 +0000

    entrypoint flesh out

--- pull-server/entrypoint.sh
+++ pull-server/entrypoint.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
+SSH_REMOTE_HOST="ph3.local"
+SSH_USERNAME="${SSH_USERNAME:-"syncoid"}"
+SSH_PORT="${SSH_PORT:-10022}"
+SSH_PRIVKEY="${SSH_PRIVKEY}"
+
+# Don't assume that these normal environment variables are set.
+if [ "${USER}" == "" ]; then
+  export USER=$(id -un)
+fi
+if [ "${HOME}" == "" ]; then
+  export HOME=$(getent passwd "${USER}" | awk 'BEGIN{FS=":"}{print $6}')
+fi
+
 #TODO: Start gitlab runner
 
 #TODO: Then what?
 while [ true ]; do
+
+# Create and populate a ~/.ssh/config file
+# TODO: This need to be expandable to multiple remote targets
+  mkdir -p "${HOME}/.ssh"
+  chmod 700 "${HOME}/.ssh"
+  cat >${HOME}/.ssh/config <<-EOF
+Host ${SSH_REMOTE_HOST}
+  HostName      chisel
+  User          ${SSH_USERNAME}
+  Port          ${SSH_PORT}
+  IdentitiyFile ${HOME}/.ssh/${SSH_USERNAME}@${SSH_REMOTE_HOST}
+EOF
+
+  echo "${SSH_PRIVKEY}" >${HOME}/.ssh/${SSH_USERNAME}@${SSH_REMOTE_HOST}
+
   sleep 3600
 done
